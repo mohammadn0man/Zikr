fe65d60b7c83fa07e18868ec5e5fa2a5d0e17592
mohammadn0man <mohammadn0man@users.noreply.github.com>
2026-09-17 17:23:33 +0530

    Refactor code structure for improved readability and maintainability

---
+++ ZikrContainer/App.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+@main
+struct ZikrContainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("This is a container for the Zikr Apple Watch app.")
+        }
+    }
+}
